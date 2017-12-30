5c9a342994fe8adbd2f6ec40bd99770a0d4be0eb
Dylan Fitzgerald <dylanfitzgerald@learnzillion.com>
2017-12-29 17:16:00 -0700

    Finish initial Elm spike

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -4,11 +4,42 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+-- MAIN
+
+
 main : Html msg
 main =
-    div [ class "games-index" ]
-        [ ul [ class "games-list" ]
-            [ li [] [ text "Platform Game" ]
-            , li [] [ text "Adventure Game" ]
-            ]
+    div []
+        [ h1 [] [ text "Games" ]
+        , gamesIndex model
         ]
+
+
+
+-- MODEL
+
+
+model : List String
+model =
+    [ "Platform Game"
+    , "Adventure Game"
+    ]
+
+
+
+-- VIEW
+
+
+gamesIndex : List String -> Html msg
+gamesIndex gameTitles =
+    div [ class "games-index" ] [ gamesList gameTitles ]
+
+
+gamesList : List String -> Html msg
+gamesList gameTitles =
+    ul [ class "games-list" ] (List.map gamesListItem gameTitles)
+
+
+gamesListItem : String -> Html msg
+gamesListItem gameTitle =
+    li [] [ text gameTitle ]

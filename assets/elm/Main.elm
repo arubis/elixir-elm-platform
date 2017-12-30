74b7c563fd66d6f1c20f26ab1684f39a25c6fbac
Dylan Fitzgerald <dylanfitzgerald@learnzillion.com>
2017-12-30 15:50:32 -0700

    Also pull Players into Main.elm and display its content

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -26,24 +26,40 @@ main =
 
 type alias Model =
     { gamesList : List Game
+    , playersList : List Player
     }
 
 
 type alias Game =
-    { title : String
-    , description : String
+    { description : String
+    , featured : Bool
+    , id : Int
+    , thumbnail : String
+    , title : String
+    }
+
+
+type alias Player =
+    { displayName : String
+    , id : Int
+    , score : Int
+    , username : String
     }
 
 
 initialModel : Model
 initialModel =
     { gamesList = []
+    , playersList = []
     }
 
 
 initialCommand : Cmd Msg
 initialCommand =
-    fetchGamesList
+    Cmd.batch
+        [ fetchGamesList
+        , fetchPlayersList
+        ]
 
 
 init : ( Model, Cmd Msg )
@@ -70,10 +86,36 @@ decodeGamesList =
 
 decodeGame : Decode.Decoder Game
 decodeGame =
-    Decode.map2 Game
-        -- n.b. this is Json.Decode.map2, not Elm.core.map2
-        (Decode.field "title" Decode.string)
+    Decode.map5 Game
+        -- n.b. this is Json.Decode.mapX, not Elm.core.mapX
+        -- RESEARCH: Json.Decode.mapX maxes out at X==8; standard practice for >=9?
         (Decode.field "description" Decode.string)
+        (Decode.field "featured" Decode.bool)
+        (Decode.field "id" Decode.int)
+        (Decode.field "thumbnail" Decode.string)
+        (Decode.field "title" Decode.string)
+
+
+fetchPlayersList : Cmd Msg
+fetchPlayersList =
+    Http.get "/api/players" decodePlayersList
+        |> Http.send FetchPlayersList
+
+
+decodePlayersList : Decode.Decoder (List Player)
+decodePlayersList =
+    decodePlayer
+        |> Decode.list
+        |> Decode.at [ "data" ]
+
+
+decodePlayer : Decode.Decoder Player
+decodePlayer =
+    Decode.map4 Player
+        (Decode.field "display_name" Decode.string)
+        (Decode.field "id" Decode.int)
+        (Decode.field "score" Decode.int)
+        (Decode.field "username" Decode.string)
 
 
 
@@ -82,6 +124,7 @@ decodeGame =
 
 type Msg
     = FetchGamesList (Result Http.Error (List Game))
+    | FetchPlayersList (Result Http.Error (List Player))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -91,6 +134,14 @@ update msg model =
             case result of
                 Ok games ->
                     ( { model | gamesList = games }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
+        FetchPlayersList result ->
+            case result of
+                Ok players ->
+                    ( { model | playersList = players }, Cmd.none )
 
                 Err _ ->
                     ( model, Cmd.none )
@@ -111,13 +162,10 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    if List.isEmpty model.gamesList then
-        div [] []
-    else
-        div []
-            [ h1 [ class "games-section" ] [ text "Games" ]
-            , gamesIndex model
-            ]
+    div []
+        [ gamesIndex model
+        , playersIndex model
+        ]
 
 
 gamesIndex : Model -> Html msg
@@ -135,4 +183,28 @@ gamesListItem game =
     li [ class "game-item" ]
         [ strong [] [ text game.title ]
         , p [] [ text game.description ]
+        ]
+
+
+playersIndex : Model -> Html msg
+playersIndex model =
+    if List.isEmpty model.playersList then
+        div [] []
+    else
+        div [ class "players-index" ]
+            [ h1 [ class "players-section" ] [ text "Players" ]
+            , playersList model.playersList
+            ]
+
+
+playersList : List Player -> Html msg
+playersList players =
+    ul [ class "players-index" ] (List.map playersListItem players)
+
+
+playersListItem : Player -> Html msg
+playersListItem player =
+    li [ class "player-item" ]
+        [ strong [] [ text player.displayName ]
+        , p [] [ text (toString player.score) ]
         ]

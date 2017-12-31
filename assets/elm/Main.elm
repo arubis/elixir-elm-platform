b10d9fc3096cbddeaef3190e4f6b89c1b4c564f0
Dylan Fitzgerald <dylanfitzgerald@learnzillion.com>
2017-12-30 22:27:14 -0700

    Add 'featured' section
    Move all non-minified styles to app.css

--- assets/elm/Main.elm
+++ assets/elm/Main.elm
@@ -165,9 +165,38 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div []
-        [ gamesIndex model
+        [ featured model
+        , gamesIndex model
         , playersIndex model
         ]
+
+
+featured : Model -> Html msg
+featured model =
+    case featuredGame model.gamesList of
+        Just game ->
+            div [ class "row featured" ]
+                [ div [ class "container" ]
+                    [ div [ class "featured-img" ]
+                        [ img [ class "featured-thumbnail", src game.thumbnail ] [] ]
+                    , div [ class "featured-data" ]
+                        [ h1 [] [ text "Featured" ]
+                        , h2 [] [ text game.title ]
+                        , p [] [ text game.description ]
+                        , button [ class "btn btn-lg btn-primary" ] [ text "Play Now!" ]
+                        ]
+                    ]
+                ]
+
+        Nothing ->
+            div [] []
+
+
+featuredGame : List Game -> Maybe Game
+featuredGame games =
+    games
+        |> List.filter .featured
+        |> List.head
 
 
 gamesIndex : Model -> Html msg
